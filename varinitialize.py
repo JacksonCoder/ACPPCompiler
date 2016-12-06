3da320c12343e7fcbafe925651f5fc491ff197f5
JacksonCoder2 <lewisfamily@Seths-Mac-mini.T-mobile.com>
2016-12-05 20:36:42 -0800

    New file added and if statements added as well

--- varinitialize.py
+++ varinitialize.py
@@ -19,7 +19,17 @@ def declarationParse(string):
                     result = parse("{:w}:{}",string)
                     name = result.fixed[0]
                     vartype = result.fixed[1]
-                    return tp.Token(vartype + ' ' + name,"dec")
+                    return tp.Token(
+                            vartype + ' ' + name,
+                            "dec"
+                            )
+                name = result.fixed[0]
+                vartype = result.fixed[1]
+                templateargs = result.fixed[2]
+                return tp.Token(
+                        vartype + '<' + templateargs + '>' + ' ' + name,
+                        "dec"
+                        )
 def isType(string):
     result = parse("({}){}",string).fixed
     if result == None:
---
+++ variousparsetools.py
@@ -0,0 +1,27 @@
+from parse import *
+import tokenparser as tp
+def isIfStatement(string):
+    result = parse("if {}",string)
+    if result == None:
+        return False
+    return True
+def isInlineIfStatement(string):
+    result = parse("if {}-> {}",string)
+    if result == None:
+        return False
+    return True
+def ifStatementParse(string)
+    result = parse("if {}")
+    boolean_value = result.fixed[0]
+    return tp.Token(
+        'if (' + boolean_value + '){',
+        'ifstatement'
+        )
+def inlineIfStatementParse(string):
+    result = parse("if {}-> {}",string)
+    boolean_value = result.fixed[0]
+    inline_if_token = tp.parseToken(result.fixed[1])
+    return tp.Token(
+            'if(' + boolean_value + ') ' + inline_if_token,
+            'inlineifstatement'
+            )
